f1a4f988227cabe408d52ce1fa54a083a62f839f
Mike Fernandez <miguel.fernandez.sc@itszapopan.edu.mx>
2015-09-23 00:44:06 -0500

    The first file of our calculato. Made functions file

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include "functions.h"
+	
+	
+	float addition (float number1, float number2)
+	{	
+		return  number1 + number2;
+	}
+
+
+	float subtraction (float number1, float number2)
+	{
+		return number1 - number2;
+	}
+
+	float division (float number1, float number2)
+	{
+		return number1 / number2;
+	}
+
+	float multiplication (float number1, float number2)
+	{
+		return number1 * number2;
+	}
+
+
